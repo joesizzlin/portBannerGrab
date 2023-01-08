2fc7cee82de3001f2a482c8d95f14db3b2939ff5
joesizzlin <cassellajoe@gmail.com>
2023-01-08 00:56:36 -0500

    fix print() order in for-loop

--- portBannerGrab.py
+++ portBannerGrab.py
@@ -8,15 +8,16 @@ Ports = [21, 22, 25, 3306]          # place the port numbers you want to scan he
 for Port in Ports:
     try:
         s = socket.socket()             # create an object named 's' instantiated from the socket class to the socket module
-        print('This is the Banner for the Port')
-        print(Port)
-        
+
         # use the connect() method from the socket module make connection to ip address and port
         # syntax is object.method (for example, socket.connect)
         s.connect(("127.0.0.1", Port))  # set to scan localhost on 'Port, change target IP as needed
 
         answer = s.recv(1024)           #use receive (recv) method to read 1024 bytes of data, contains the banner information, assign as answer
 
+        print('This is the Banner for the Port: ')
+        print(Port)
+        
         print(answer)                       
 
         s.close()
